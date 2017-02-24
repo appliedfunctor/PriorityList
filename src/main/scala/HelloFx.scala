47f8bd1d250669562b0e6ea7cf592e2b583e05ab
Alexander Worton <kesomir@btinternet.com>
2017-02-24 20:15:10 +0000

    Scaling issue seen on added label.

--- src/main/scala/HelloFx.scala
+++ src/main/scala/HelloFx.scala
@@ -1,5 +1,8 @@
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.{Group, Scene}
+import scalafx.scene.control.Label
+import scalafx.scene.text.Font
 /**
   * Created by aworton on 24/02/17.
   */
@@ -8,4 +11,8 @@ object HelloFx extends JFXApp{
   stage = new PrimaryStage
   stage.setTitle("Hello ScalaFX")
 
+  val group = new Group(new Label("Hello Label"){styleClass = Iterable("scale")})
+  val scene = new Scene(group, 800, 600)
+  scene.stylesheets.add("main.css")
+  stage.scene = scene
 }
